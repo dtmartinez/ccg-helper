1561621662a4ee1c41a195d5cc4cc60a4b901225
dtmartinez <diegotm.90@gmail.com>
2025-09-27 12:09:02 +0200

    Initial parent repository commit

---
+++ card-service/src/main/kotlin/ccghelper/cardservice/CardServiceApplication.kt
@@ -0,0 +1,11 @@
+package ccghelper.cardservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CardServiceApplication>(*args)
+}
---
+++ card-service/src/test/kotlin/ccghelper/cardservice/CardServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ccghelper.cardservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
